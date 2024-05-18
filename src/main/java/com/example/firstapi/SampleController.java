61933946b2c3513acb6ace7feba3d1539c0ccffd
Chithreshwar <chithuprabha2407@gmail.com>
2024-05-18 19:47:00 +0530

    spring boot hello world

---
+++ src/main/java/com/example/firstapi/SampleController.java
@@ -0,0 +1,21 @@
+package com.example.firstapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class SampleController {
+
+    @GetMapping("/sayhello")
+    public String sayHello(){
+        return "Hello, Everyone";
+    }
+
+    @GetMapping("/saybye/{name}")
+    public String sayBye(@PathVariable("name") String name){
+        return "Bye " + name + " Please come again!";
+    }
+}
